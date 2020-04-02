14465f7b2735cd9f60570a405d04482bfc347586
SenaGust <gustavosena98@live.com>
2020-04-02 19:34:47 -0300

    Updates

---
+++ precache-manifest.e2c663dfb0af497c50ad29f6b91a0865.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "111bea0a87a58c811f55e2df5fafead8",
+    "url": "/react-training/index.html"
+  },
+  {
+    "revision": "2efcf2198e697362f879",
+    "url": "/react-training/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "463368783c50c22a8488",
+    "url": "/react-training/static/js/2.25668966.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-training/static/js/2.25668966.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2efcf2198e697362f879",
+    "url": "/react-training/static/js/main.fb74d892.chunk.js"
+  },
+  {
+    "revision": "c10a57e5d6c88fc58da3",
+    "url": "/react-training/static/js/runtime-main.d2ebcd1f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-training/static/media/logo.5d5d9eef.svg"
+  }
+]);
