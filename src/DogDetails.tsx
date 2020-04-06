b615405dadcb32d88fae89fbbf1ae22f10962178
SenaGust <gustavosena98@live.com>
2020-04-06 14:20:42 -0300

    component using function

---
+++ src/DogDetails.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+interface Props{
+  name: string,
+  photo: string
+}
+
+function DogDetailsView(props: Props) {
+  return (
+    <div>
+      <img  ref="image" src={props.photo} />
+      <h1>{props.name}</h1>
+      <button> Bark! </button>
+    </div>
+  );
+}
+
+export default DogDetailsView;
