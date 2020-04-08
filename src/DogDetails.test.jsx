3713caba2daddf4e63c0defa31769ba0eb93a72a
SenaGust <gustavosena98@live.com>
2020-04-08 16:34:40 -0300

    correct the code

--- src/DogDetails.test.jsx
+++ src/DogDetails.test.jsx
@@ -1,10 +1,10 @@
 import DogDetailsView from './DogDetails';
-import { , shallow } from 'enzyme';
+import { shallow } from 'enzyme';
 import React from 'react';
 
 describe('function DogDetailsView', () => {
     it('return Roberto e uma imagem', () => {
-        const wrapper = shallow(<DogDetailsView name="Roberto" photo='https://img.ibxk.com.br/2019/07/16/16123213283052.jpg?w=1120&h=420&mode=crop&scale=both'/>);
+        const wrapper = shallow(<DogDetailsView name="Roberto" photo='https://img.ibxk.com.br/2019/07/16/16123213283052.jpg'/>);
         
         const propsExpect = wrapper.props();
 
