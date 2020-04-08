9d2ce3cbf77509fd3c84aee5216960493db01c1d
SenaGust <gustavosena98@live.com>
2020-04-08 15:00:40 -0300

    unit test

---
+++ src/DogDetails.test.jsx
@@ -0,0 +1,14 @@
+import DogDetailsView from './DogDetails';
+import { , shallow } from 'enzyme';
+import React from 'react';
+
+describe('function DogDetailsView', () => {
+    it('return Roberto e uma imagem', () => {
+        const wrapper = shallow(<DogDetailsView name="Roberto" photo='https://img.ibxk.com.br/2019/07/16/16123213283052.jpg?w=1120&h=420&mode=crop&scale=both'/>);
+        
+        const propsExpect = wrapper.props();
+
+        expect(propsExpect.name).toBe('fieldName');
+        expect(propsExpect.photo).toBe('fieldName');
+    });
+});
