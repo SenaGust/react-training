c1b85ee64dbff7f055314f8a203c6d043b07f29e
SenaGust <gustavosena98@live.com>
2020-04-23 15:31:41 -0300

    removed unused import

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import DogDetails from './DogDetails'
-import Swal from 'sweetalert2';
 import CreateBeerForm from './CreateBeerForm'
 
 function App() {
