169a11e525f70c192a5ffb671c7344fb5de72948
SenaGust <gustavosena98@live.com>
2020-04-23 15:51:24 -0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -9,8 +9,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
 
-        <CreateBeerForm />
       </header>
+      <CreateBeerForm />
     </div>
   );
 }
