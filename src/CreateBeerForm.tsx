c616169adf6d682551f4b0416aa7d4c0c00b777d
SenaGust <gustavosena98@live.com>
2020-04-27 12:21:38 -0300

    console.log() with values form

--- src/CreateBeerForm.tsx
+++ src/CreateBeerForm.tsx
@@ -5,13 +5,18 @@ import CreateBeerFormInputText from './Form/CreateBeerFormInputText';
 import CreateBeerFormTextArea from './Form/CreateBeerFormTextArea';
 
 function CreateBeerForm() {
+    const onSubmit = (event: BaseSyntheticEvent) => {
+        event.preventDefault();
+        console.log(valueTextArea, valueInputText, valueComboBox, valueCheckBox);
+    };
+
     const [valueTextArea, onChangeTextArea] = useState("");
     const [valueInputText, onChangeInputText] = useState("");
     const beerTypes = ['Ale', 'Lager', 'Stout'];
     const [valueComboBox, onChangeComboBox] = useState(beerTypes[0]);
     const [valueCheckBox, onChangeCheckBox] = useState(false);
     return (
-        <form>
+        <form onSubmit={onSubmit}>
             <h1>Beer form</h1>
             <CreateBeerFormInputText label="Beer name:" value={valueInputText} onChange={e => onChangeInputText(e.target.value)}/>
             <CreateBeerFormCombobox label="Beer type:" elements={beerTypes} value={valueComboBox} onChange={e => onChangeComboBox(e.target.value)}/>
