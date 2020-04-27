b12f7f676332872a2e888bc841052c920526c521
SenaGust <gustavosena98@live.com>
2020-04-27 13:40:51 -0300

    uptade order

--- src/App.tsx
+++ src/App.tsx
@@ -8,9 +8,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-
+    
+        <CreateBeerForm />
       </header>
-      <CreateBeerForm />
     </div>
   );
 }
