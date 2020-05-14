926b7141f71cb106c75b7bf7dd2154abc1a8f9d6
SenaGust <gustavosena98@live.com>
2020-05-14 15:04:25 -0300

    Removed unused const

--- src/CreateBeerFormik/CreateBeerFormik.test.jsx
+++ src/CreateBeerFormik/CreateBeerFormik.test.jsx
@@ -1,28 +1,13 @@
-import {shallow, mount} from 'enzyme';
+import {shallow} from 'enzyme';
 import React from 'react';
 import {Formik} from 'formik';
 
 import CreateBeerFormik from './CreateBeerFormik';
-import FormikCombobox from '../Formik/FormikCombobox';
-import FormikCheckbox from '../Formik/FormikCheckbox';
-import FormikInputText from '../Formik/FormikInputText';
-import FormikTextArea from '../Formik/FormikTextArea';
 
 describe('function CreateBeerFormik', () => {
     it('should show components properly', () => {
         let wrapper = shallow(<CreateBeerFormik />);
-        const form = ({ handleSubmit }) => (
-            <form onSubmit={handleSubmit}>
-                <h1>Beer form</h1>
-                <FormikInputText label="Beer name:" name="beerName"/>
-                <FormikCombobox label="Beer type:" elements={beerTypes} name="beerType"/>
-                <FormikCheckbox label="Has corn" name="hasCorn"/>
-                <FormikTextArea label="Ingredients" name="ingredients"/>
-                <button type="submit">Submit</button>
-            </form>
-        );
         expect(wrapper.type()).toBe(Formik);
-        //expect(wrapper.childAt(0)).toBe(form);
     });
     it('should submit values', () => {
         const values = {beerName: '', beerType: '', hasCorn: false, ingredients: ''}
