bce8818c0c8b25e859972566bcf5783cee38bfc8
SenaGust <gustavosena98@live.com>
2020-05-15 15:50:50 -0300

    implemented unit test

--- src/components/formikCheckbox/FormikCheckbox.test.jsx
+++ src/components/formikCheckbox/FormikCheckbox.test.jsx
@@ -5,13 +5,9 @@ import {FastField} from 'formik'
 
 describe('function FormikCheckbox', () => {
     const label = "has corn";
-    const onChange = jest.fn().mockImplementation(() => {
-        value = !value;
-    });
-    let value = true;
 
-    it('children should be properly orderly', () => {
-        const wrapper = shallow(<FormikCheckbox label={label} value={value} onChange={onChange}/>);
+    it('should be properly orderly', () => {
+        const wrapper = shallow(<FormikCheckbox label={label} name="hasCorn"/>);
 
         expect(wrapper.type()).toEqual('div');
         
@@ -20,15 +16,7 @@ describe('function FormikCheckbox', () => {
         expect(labelWrapper.children()).toHaveLength(2);
         expect(labelWrapper.childAt(1).text()).toBe(label);
 
-        const inputMock = (<Field onClick={onChange} checked={true} type="checkbox" />);
+        const inputMock = (<FastField type="checkbox" name="hasCorn" />);
         expect(labelWrapper.childAt(0).matchesElement(inputMock)).toBeTruthy();
-    });
-    it('should update the checkbox to false when I click on the checkbox', () => {
-        const wrapper = shallow(<FormikCheckbox label={label} value={value} onChange={onChange}/>);
-
-        wrapper.find('Field').simulate('click');
-
-        expect(onChange).toBeCalledTimes(1);
-        expect(value).toBeFalsy();
     });
 });
