802b05d46cec3203a71af0d60d5ec5d2765ec59c
SenaGust <gustavosena98@live.com>
2020-06-04 14:44:59 -0300

    Replaced Select HTML to Select Material UI

--- src/components/formikCombobox/FormikCombobox.test.jsx
+++ src/components/formikCombobox/FormikCombobox.test.jsx
@@ -2,6 +2,7 @@ import FormikCombobox from './FormikCombobox';
 import { shallow } from 'enzyme';
 import React from 'react';
 import {FastField} from 'formik';
+import {Select} from '@material-ui/core'
 
 describe('function formikCombobox', () => {
 
@@ -9,8 +10,8 @@ describe('function formikCombobox', () => {
     it('children should be properly orderly', () => {
         const label = "Beer type: ",
         name = "beerType",
-        elements = ['Ale', 'Beer'];
-        const wrapper = shallow(<FormikCombobox label={label} elements={elements} name={name}/>);
+        elementsMock = ['Ale', 'Beer'];
+        const wrapper = shallow(<FormikCombobox label={label} elementsMock={elementsMock} name={name}/>);
 
         expect(wrapper.type()).toEqual('div');
         expect(wrapper.children()).toHaveLength(1);
@@ -18,14 +19,29 @@ describe('function formikCombobox', () => {
         expect(labelWrapper.type()).toEqual('label');
         expect(labelWrapper.children()).toHaveLength(2);
         expect(labelWrapper.childAt(0).text()).toBe(label);
-        const inputMock = (<FastField component="select" name={name}>
-        { elements.map(item => (
-                <option key={item} value={item}>
-                    {item}
-                </option>
-            ))}
-    </FastField>);
-        expect(labelWrapper.childAt(1).matchesElement(inputMock)).toBeTruthy();
+        const selectMock = (
+            <FastField name={name}>
+            { (fieldProps) => {
+                return (
+                <Select
+                    value={fieldProps.field.value}
+                    fullWidth
+                    onChange={fieldProps.field.onChange}
+                    inputProps={{
+                        name: fieldProps.field.name
+                    }}>
+                       {
+                           elementsMock.map(item => (
+                            <option key={item} value={item}>
+                                {item}
+                            </option>
+                        ))
+                       }
+                </Select>);
+            }}
+        </FastField>
+        );
+        expect(labelWrapper.childAt(1).matchesElement(selectMock)).toBeTruthy();
     
     });
 });
