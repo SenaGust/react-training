540202fc77e82ab524194cfa05a77b44dabc678b
SenaGust <gustavosena98@live.com>
2020-06-05 13:50:00 -0300

    update TextareaAutosize to TextField multiline

--- src/components/formikTextArea/FormikTextArea.test.jsx
+++ src/components/formikTextArea/FormikTextArea.test.jsx
@@ -2,7 +2,7 @@ import FormikTextArea from './FormikTextArea';
 import { shallow } from 'enzyme';
 import React from 'react';
 import {FastField} from 'formik';
-import {TextareaAutosize} from '@material-ui/core';
+import {TextField} from '@material-ui/core';
 
 describe('function formikTextArea', () => {
     const labelMock = "ingredients",
@@ -12,22 +12,21 @@ describe('function formikTextArea', () => {
         const wrapper = shallow(<FormikTextArea label={labelMock} name={nameMock} />);
 
         expect(wrapper.type()).toEqual('div');
-        expect(wrapper.childAt(0).type()).toEqual('label');
+        expect(wrapper.childAt(0).type()).toEqual(FastField);
         
-        const labelWrapper = wrapper.childAt(0);
-        expect(labelWrapper.children()).toHaveLength(2);
-
-        expect(labelWrapper.childAt(0).text()).toEqual(labelMock);
-
-        const textAreaWrapper = labelWrapper.childAt(1);
+        const textAreaWrapper = wrapper.childAt(0);
+        expect(textAreaWrapper.children()).toHaveLength(1);
 
         const textAreaMock = (
             <FastField type="text" name={nameMock}>
                     {
                         (fieldProps) => {
                             return (
-                                <TextareaAutosize 
-                                rowsMin={2}
+                                <TextField
+                                rows={2}
+                                multiline
+                                label={labelMock}
+                                variant="outlined"
                                 name={fieldProps.field.name}
                                 value={fieldProps.field.value}
                                 onChange={fieldProps.field.onChange}
--- src/components/formikTextArea/FormikTextArea.tsx
+++ src/components/formikTextArea/FormikTextArea.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {FastField, FieldProps} from 'formik';
-import {TextareaAutosize} from '@material-ui/core'
+import {TextField} from '@material-ui/core'
 
 interface Props{
     label: string,
@@ -10,24 +10,24 @@ interface Props{
 function FormikTextArea(props: Props) {
     return (
         <div>
-            <label> 
-                {props.label}
-                <FastField type="text" name={props.name}>
-                    {
-                        (fieldProps: FieldProps) => {
-                            return (
-                                <TextareaAutosize
-                                rowsMin={2}
-                                name={fieldProps.field.name}
-                                value={fieldProps.field.value}
-                                onChange={fieldProps.field.onChange}
-                                onBlur={fieldProps.field.onBlur}
-                                />
-                            );
-                        }
-                    }    
-                </FastField>
-            </label>
+            <FastField type="text" name={props.name}>
+                {
+                    (fieldProps: FieldProps) => {
+                        return (
+                            <TextField
+                            rows={2}
+                            multiline
+                            label={props.label}
+                            variant="outlined"
+                            name={fieldProps.field.name}
+                            value={fieldProps.field.value}
+                            onChange={fieldProps.field.onChange}
+                            onBlur={fieldProps.field.onBlur}
+                            />
+                        );
+                    }
+                }    
+            </FastField>
         </div>
     );
 }
